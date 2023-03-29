5928bfdaf0bbea12b589d5a3c08151096007db20
Satyam Patel <satya@dyn237034.cc.lehigh.edu>
2023-03-29 15:52:22 -0400

    removed .next directory

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/projects/lutron":["static\u002Fchunks\u002Fpages\u002Fprojects\u002Flutron.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fprojects\u002Flutron"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
